098c10bd3a14c719ec392febae6215c53fb09a3f
warkadegaurav770-del <warkadegaurav770@gmail.com>
2026-06-22 18:36:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
-from flask import Flask, request, jsonify
-from youtube_transcript_api import YouTubeTranscriptApi
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "API Running"
-
 @app.route("/transcript")
 def transcript():
     video_id = request.args.get("videoId")
 
     try:
-        transcript = YouTubeTranscriptApi.get_transcript(video_id)
-        text = " ".join([x["text"] for x in transcript])
+        ytt_api = YouTubeTranscriptApi()
+        transcript = ytt_api.fetch(video_id)
+
+        text = " ".join([x.text for x in transcript])
 
         return jsonify({
             "success": True,
